f2ccf21e852a3932e75a674e0bb51f0b862d0886
johneckert <johnteckert@gmail.com>
2018-01-14 17:10:08 -0500

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+
+  resources :students
+
+  resources :school_classes
+
+  # patch '/schoolclasses/:id', to: 'schoolclasses#update'
+
+  # delete '/schoolclass/:id', to: 'schoolclass#delete'
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
